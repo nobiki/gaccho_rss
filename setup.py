45027b3f3c27acc2ec5c19f036c61cc92a4c6610
Naoaki Obiki <obiki.wk@gmail.com>
2017-10-20 15:43:02 +0900

    init.

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'gaccho_twitter',
+        version          = '0.0.1',
+        description      = 'Gaccho Twitter Plugin',
+        license          = 'MIT',
+        author           = 'nobiki',
+        author_email     = 'test@example.com',
+        url              = 'https://github.com/nobiki/gaccho_twitter.git',
+        keywords         = 'gaccho twitter',
+        packages         = find_packages(),
+        install_requires = [],
+        )
+
