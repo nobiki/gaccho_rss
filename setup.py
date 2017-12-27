caaba667c53b31ef516c2ed8efcd2fed8ccedb82
Naoaki Obiki <obiki.wk@gmail.com>
2017-12-27 14:41:41 +0900

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name             = 'gaccho_rss',
-        version          = '0.0.3',
+        version          = '0.1',
         description      = 'Gaccho Rss Feed Plugin',
         license          = 'MIT',
         author           = 'nobiki',
